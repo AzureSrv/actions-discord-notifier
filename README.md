056a5f4ae984e4984a1409cd5ed63895a91ea234
Azure-Agst <me@azureagst.dev>
2022-12-05 14:59:43 -0500

    Push v1.rc1 to GitHub

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Discord Actions Notifier
+
+[![Test](https://github.com/AzureSrv/actions-discord-notifier/actions/workflows/test.yml/badge.svg)](https://github.com/AzureSrv/actions-discord-notifier/actions/workflows/test.yml)
+
+An action to send a Discord Webhook in a Github Actions workflow. Automatically wrapped within an embed.
+
+## Inputs
+
+| Name       | Type     | Description           |
+|------------|----------|-----------------------|
+| `webhook`  | String   | Discord Webhook URL   |
+| `message`  | String   | Your escaped message  |
+
+## Example
+
+```yaml
+on: push
+
+jobs:
+  test:
+    name: Test-Action
+    runs-on: ubuntu-20.04
+    steps:
+
+      - name: Send Test Message
+        id: test
+        uses: azuresrv/actions-discord-notifier@v1
+        with:
+          webhook: ${{ secrets.WEBHOOK }}
+          message: This is a test message!
+```
---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# make sure wenhook and messages are defined
+if [[ ${webhook:-none} == "none" || ${message:-none} == "none" ]]; then
+    echo "::error Webhook or Message not defined! Exiting..."
+    exit -1
+fi
+
+# format embed
+json=$(cat <<EOF
+{
+  "content": "",
+  "embeds": [
+    {
+      "title": "[${repo#*/}:${ref_name}] Action Notif",
+      "description": "$message",
+      "url": "https://github.com/$repo/actions/runs/$run_id",
+      "color": 16777215,
+      "author": {
+        "name": "$owner",
+        "icon_url": "https://github.com/${owner}.png"
+      },
+      "footer": {
+        "text": "Run #${run_number}"
+      },
+      "timestamp": "$(date +"%Y-%m-%dT%H:%M:%S%z")"
+    }
+  ],
+  "attachments": []
+}
+EOF
+)
+
+# minify and validate
+minified=$(jq -r tostring <<< $json)
+if [[ $? -ne 0 ]]; then
+    echo "::error::Failed parsing JSON! Make sure your message is escaped properly?"
+    exit 1
+fi
+
+# send it off
+curl -X POST \
+  $webhook?wait=true \
+  -H 'Content-Type: application/json' \
+  -d "$minified"
